0eed7118eaea589510a1ec94fcac49f5ecf2e795
Bruno Carvalho <brunobcarvalho@live.com>
2021-02-19 23:10:49 -0300

    script rodando tudo

--- testsBatch.sh
+++ testsBatch.sh
@@ -2,6 +2,13 @@
 
 g++ common.cpp node.cpp dynamicProgMulMatrix.cpp -o batchCompiled
 
+echo "method;numIt;instanceSize;avgNumOp;avgTime;avgMem;" >> ./results/bruteForce.csv
+
+for f in ./instancias/bruteForce/*.csv; do
+        echo "Executando Testes para o arquivo" "$f" "..."
+        ./batchCompiled $f 1 2 >> ./results/bruteForce.csv
+done
+
 echo "method;numIt;instanceSize;avgNumOp;avgTime;avgMem;" >> ./results/dynamic.csv
 
 for f in ./instancias/dynamicProgramming/*.csv; do
