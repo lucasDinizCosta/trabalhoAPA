c4872441df2a1b2d103c1175ae48fbb695c83c24
Bruno Carvalho <brunobcarvalho@live.com>
2021-02-19 22:37:47 -0300

    teste dynamic

--- testBatch.sh
+++ testBatch.sh
@@ -2,9 +2,9 @@
 
 g++ common.cpp node.cpp dynamicProgMulMatrix.cpp -o batchCompiled
 
-echo "method;numIt;instanceSize;avgNumOp;avgTime;avgMem;" >> ./results/bruteForce.csv
+echo "method;numIt;instanceSize;avgNumOp;avgTime;avgMem;" >> ./results/dynamic.csv
 
-for f in ./instancias/bruteForce/*.csv; do
-        echo "Executando Testes para o arquivo " "$f" "..."
-        ./batchCompiled $f 1 2 >> ./results/bruteForce.csv
+for f in ./instancias/dynamicProgramming/*.csv; do
+        echo "Executando Testes para o arquivo" "$f" "..."
+        ./batchCompiled $f 2 2 >> ./results/dynamic.csv
 done
