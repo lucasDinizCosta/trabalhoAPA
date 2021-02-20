580ca62d374fd966c41fb9d8487a48a3602808b6
Bruno Carvalho <brunobcarvalho@live.com>
2021-02-19 21:55:16 -0300

    Comentado o que só roda no linux

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+double vmInit, rssInit;              // Armazena a memória utilizada ao inicio da execucao
+     // double vmEnd, rssEnd;                // Armazena a memória utilizada ao final da execucao
+     // process_mem_usage(vmInit, rssInit);  // Calcula a memoria utilizada e seta no inicio
