81d52373c5264068f52b5ddafb3932d25776db06
Bruno Carvalho <brunobcarvalho@live.com>
2021-02-19 22:07:01 -0300

    removendo arquivos temp

--- tempCodeRunnerFile.cpp
+++
@@ -1,3 +0,0 @@
-double vmInit, rssInit;              // Armazena a memória utilizada ao inicio da execucao
-     // double vmEnd, rssEnd;                // Armazena a memória utilizada ao final da execucao
-     // process_mem_usage(vmInit, rssInit);  // Calcula a memoria utilizada e seta no inicio
