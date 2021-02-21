638d98597be9166ebc9bb84c4976117e0bcc009b
Bruno Carvalho <brunobcarvalho@live.com>
2021-02-21 12:01:25 -0300

    novo script python

--- results/graphics.py
+++ results/graphics.py
@@ -1,23 +1,40 @@
-import matplotlib.pyplot as plt
+import numpy as np
+import pylab as pl
 import pandas as pd
 
-plt.style.use('bmh')
+f = pd.read_csv("dynamic.csv", sep=';')
+f2 = pd.read_csv("recursive.csv", sep=';')
+
+x = f2['instanceSize']
+y_ori = f2['avgTime']
 
 
-recursive = pd.read_csv('C:/Git/Trabalho de APA/trabalhoAPA/results/recursive.csv', ';')
+#interpolation functions
+coeficientes = np.polyfit (x, y_ori, 1)
+pol1 = np.poly1d(coeficientes)
+y_lin =   coeficientes[0]*x + coeficientes[1]
+pl.plot(x, y_lin, ms=6, label='Linear')
 
-dynamic = pd.read_csv('C:/Git/Trabalho de APA/trabalhoAPA/results/dynamic.csv', ';')
+coeficientes = np.polyfit (x, y_ori, 2)
+pol1 = np.poly1d(coeficientes)
+y_quad = coeficientes[0]*x**2  + coeficientes[1]*x + coeficientes[2]
+pl.plot(x, y_quad, ms=6, label='Quad')
 
-x = recursive['instanceSize']
-y = recursive['avgTime']
+coeficientes = np.polyfit (x, y_ori, 3)
+pol1 = np.poly1d(coeficientes)
+y_cubic = coeficientes[0]*x**3 + coeficientes[1]*x**2  + coeficientes[2]*x + coeficientes[3]
+pl.plot(x, y_cubic, ms=6, label='Cubica')
+ 
+coeficientes = np.polyfit (x, np.log(y_ori), 1, w=np.sqrt(y_ori))
+pol1 = np.poly1d(coeficientes)
+y_exp = (np.exp(1)**coeficientes[1])*(np.exp(1)**(coeficientes[0]*x))
+pl.plot(x, y_exp, ms=6, label='Exp', color='purple')
 
-plt.title('Algoritmo Recursivo: Tamanho da Instância x Tempo Gasto')
-plt.xlabel("Tamanho da Instância", fontsize=18)
-plt.ylabel('Média de Tempo Gasto (segundos)', fontsize=16)
+pl.xlim(x.min(), x.max())
 
-# Limits for the Y axis
-# plt.ylim(0, 800)
-
-plt.plot(x, y, color = (0.9,0.2,0.2,0.8))
-
-plt.show()
+pl.ylabel("Média de Tempo Gasto (segundos)")
+pl.xlabel("Tamanho da instância")
+pl.title("Algoritmo Recursivo: Tamanho da Instância x Tempo de Execução")
+pl.plot(x, y_ori,'o',ms=6, label='Medições', color='red')
+pl.legend()
+pl.show()
