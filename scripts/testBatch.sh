6a70cb9fcd99d955a194be4ada2263b649d09899
Bruno Carvalho <brunobcarvalho@live.com>
2021-02-21 12:00:43 -0300

    ajuste script

--- scripts/testBatch.sh
+++ scripts/testBatch.sh
@@ -4,14 +4,14 @@ g++ ../common.cpp ../node.cpp ../dynamicProgMulMatrix.cpp -o batchCompiled
 
 echo "method;numIt;instanceSize;avgNumOp;avgTime;avgMem;" >> ../results/recursiveSolution.csv
 
-for f in ../instancias/recursiveSolution/.csv; do
+for f in ../instancias/recursiveSolution/*.csv; do
         echo "Executando Testes para o arquivo" "$f" "..."
         ./batchCompiled $f 1 2 >> ../results/recursiveSolution.csv
 done
 
 echo "method;numIt;instanceSize;avgNumOp;avgTime;avgMem;" >> ../results/dynamic.csv
 
-for f in ../instancias/dynamicProgramming/.csv; do
+for f in ../instancias/dynamicProgramming/*.csv; do
         echo "Executando Testes para o arquivo" "$f" "..."
         ./batchCompiled $f 2 2 >> ../results/dynamic.csv
 done
